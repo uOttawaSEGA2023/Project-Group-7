e61745f9a72501800752cc97d68f37422a197e90
SplitWolf <mat.jake1360@gmail.com>
2023-09-24 21:43:53 -0400

    feat(admin): Add Basic Admin class. Example User subclass

---
+++ app/src/main/java/com/seg2105a/projectgroup7/hams/admin/Administrator.java
@@ -0,0 +1,36 @@
+package com.seg2105a.projectgroup7.hams.admin;
+
+import com.google.firebase.firestore.Blob;
+import com.seg2105a.projectgroup7.hams.user.User;
+import com.seg2105a.projectgroup7.hams.user.UserType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Administrator extends User {
+
+    private static final Administrator instance = new Administrator();
+    private boolean isRegistered = false;
+    private Administrator() {
+        super("Admin", "", "12345".toCharArray(), "Administrator", "000-000-000", "", UserType.ADMIN);
+        this.signUp();
+    }
+
+    @Override
+    protected void signUp() {
+        if(isRegistered) return;
+        Map<String,Object> user = new HashMap<>();
+        user.put("username","Admin");
+        user.put("password", Blob.fromBytes(this.getPassword()));
+        User.registeredUsers.add(user);
+        isRegistered = true;
+    }
+    public static Administrator getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void changeView() {
+
+    }
+}
