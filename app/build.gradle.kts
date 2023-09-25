6bb3506606293b3fb999fd00b82e4bb7df23d3e9
SplitWolf <mat.jake1360@gmail.com>
2023-09-24 21:34:43 -0400

    build(firebase): Add Firebase to the gradle project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,14 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.seg2015a.projectgroup7.hams"
+    namespace = "com.seg2105a.projectgroup7.hams"
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.seg2015a.projectgroup7.hams"
+        applicationId = "com.seg2105a.projectgroup7.hams"
         minSdk = 24
         targetSdk = 33
         versionCode = 1
@@ -33,6 +34,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation("com.google.firebase:firebase-firestore")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
