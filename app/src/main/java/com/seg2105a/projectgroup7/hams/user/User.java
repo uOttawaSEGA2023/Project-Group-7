7737151e81d7f7784d57ca37f0fcc9ec079f3bb4
SplitWolf <mat.jake1360@gmail.com>
2023-09-28 17:12:30 -0400

    feat(user): Change user class implementation to follow the 3 list seperation architetcure.
    Initilized the lists for storage

---
+++ app/src/main/java/com/seg2105a/projectgroup7/hams/user/User.java
@@ -0,0 +1,209 @@
+package com.seg2105a.projectgroup7.hams.user;
+
+import android.util.Log;
+
+import com.seg2105a.projectgroup7.hams.admin.Administrator;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+public abstract class User {
+
+   // private static final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public static List<Map<String,Object>> registeredPatients = new LinkedList<>();
+    public static List<Map<String,Object>> registeredDoctors = new LinkedList<>();
+    private final UserType _userType;
+    private String _firstName;
+    private String _lastName;
+
+
+    private byte[] _hashedPassword;
+    private String _email;
+    private String _phone;
+    private String _address;
+
+
+//    public static void read() {
+//        String TAG = "Firebase";
+//        db.collection("users")
+//                .get()
+//                .addOnCompleteListener(task -> {
+//
+//                    if (task.isSuccessful()) {
+//                        for (QueryDocumentSnapshot document : task.getResult()) {
+//                            Log.d(TAG, document.getId() + " => " + document.getData());
+//                        }
+//                    } else {
+//                        Log.w(TAG, "Error getting documents.", task.getException());
+//                    }
+//                });
+//    }
+
+    public User(String firstName, String lastName, char[] password, String email, String phone, String address, UserType userType) {
+        _firstName = firstName;
+        _lastName = lastName;
+        _hashedPassword = hashPassword(password);
+        _email = email;
+        _phone = phone;
+        _address = address;
+        _userType = userType;
+        Log.w("pass", Arrays.toString(User.hashPassword("123".toCharArray())));
+    }
+    public User(String firstName, String lastName, byte[] hashedPassword, String email, String phone, String address, UserType userType) {
+        _firstName = firstName;
+        _lastName = lastName;
+        _hashedPassword = hashedPassword;
+        _email = email;
+        _phone = phone;
+        _address = address;
+        _userType = userType;
+    }
+
+
+    private static byte[] hashPassword(char[] password) {
+        byte[] salt = new byte[16];
+        SecureRandom random = new SecureRandom();
+        random.nextBytes(salt);
+        KeySpec spec = new PBEKeySpec(password, salt, 65536, 128);
+        try {
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+
+            return factory.generateSecret(spec).getEncoded();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException ignored) {
+
+        }
+        return new byte[0];
+    }
+
+    /*
+    * signUp should generate a map with strings for keys and objects for values
+    * the keys should match the names of the private variables, with the underscore removed.
+    * And then this map should be added to the static registeredUsers list in this class
+    **/
+    protected abstract void signUp();
+    protected abstract void changeView();
+
+    //TODO: Remove login return codes.
+    public static LoginReturnCodes login(String email, char[] password, UserType userType) {
+
+        Map<String, Object> userData = null;
+        User loggedInUser = null;
+        switch (userType) {
+            case DOCTOR:
+                userData = searchLoop(registeredDoctors,email);
+                if(userData != null) {
+                    if(checkPassword(password,(byte[]) userData.get("password"))) {
+                        //loggedInUser = new Doctor();
+                    } else {
+                        return LoginReturnCodes.IncorrectPassword;
+                    }
+                }
+                break;
+            case PATIENT:
+                userData = searchLoop(registeredPatients,email);
+                if(userData != null) {
+                    if(checkPassword(password,(byte[]) userData.get("password"))) {
+                        //loggedInUser = new Doctor();
+                    } else {
+                        return LoginReturnCodes.IncorrectPassword;
+                    }
+                }
+                break;
+            case ADMIN:
+                if(Administrator.getInstance().getEmail().equals(email)) {
+                    if(checkPassword(password,(byte[]) userData.get("password"))) {
+                        loggedInUser = Administrator.getInstance();
+                    } else {
+                        return LoginReturnCodes.IncorrectPassword;
+                    }
+                } else {
+                    return LoginReturnCodes.UserDoesNotExist;
+                }
+                break;
+        }
+        loggedInUser.changeView();
+        return LoginReturnCodes.UserDoesNotExist;
+    }
+    private static Map<String, Object> searchLoop(List<Map<String, Object>> toSearch, String emailToSearch) {
+        for (Map<String, Object> userData : toSearch) {
+            if(!Objects.equals(userData.get("email"), emailToSearch)) continue;
+            return userData;
+        }
+        return null;
+    }
+    private static boolean checkPassword(char[] password, byte[] hashedPassword) {
+        return hashPassword(password) == hashedPassword;
+    }
+
+    //<editor-fold desc="Getters & Setters">
+    public String getFirstName() {
+        return _firstName;
+    }
+
+    public User setFirstName(String firstName) {
+        _firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return _lastName;
+    }
+
+    public User setLastName(String lastName) {
+        _lastName = lastName;
+        return this;
+    }
+
+    public byte[] getPassword() {
+        return _hashedPassword;
+    }
+
+    public User setPassword(char[] oldPassword, char[] newPassword) {
+        if(hashPassword(oldPassword).equals(_hashedPassword)) {
+            _hashedPassword = hashPassword(newPassword);
+        }
+        return this;
+    }
+
+    public String getEmail() {
+        return _email;
+    }
+
+    public User setEmail(String email) {
+        _email = email;
+        return this;
+    }
+
+    public String getPhone() {
+        return _phone;
+    }
+
+    public User setPhone(String phone) {
+        _phone = phone;
+        return this;
+    }
+
+    public String getAddress() {
+        return _address;
+    }
+
+    public User setAddress(String address) {
+        _address = address;
+        return this;
+    }
+    public UserType getUserType() {
+        return _userType;
+    }
+    // </editor-fold>
+}
