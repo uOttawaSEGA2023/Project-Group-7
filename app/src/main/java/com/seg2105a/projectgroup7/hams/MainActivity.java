7d8718eba8797476e07f51902904da612f4e4954
ARelaxedScholar <angemmanuel.kouakou+professional@gmail.com>
2023-10-01 21:12:18 -0400

    feat(MainActivity): Created the registration form for Patient (XML)
    Created the register form and selection screens to bring to the needed screen. Also altered MainActivity.java so that it manages the screen swapping. Still requires connecting to Patient class.

--- app/src/main/java/com/seg2105a/projectgroup7/hams/MainActivity.java
+++ app/src/main/java/com/seg2105a/projectgroup7/hams/MainActivity.java
@@ -3,6 +3,10 @@ package com.seg2105a.projectgroup7.hams;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void openSignUp(View view){
+        setContentView(R.layout.signup_acount_type_selection_view);
+    }
+
+    public void clickedContinueRegistration(View view){
+        RadioGroup accountTypeSelection = findViewById(R.id.userTypeSelection);
+        if (accountTypeSelection.getCheckedRadioButtonId() == -1){
+            Toast.makeText(getApplicationContext(), "Please select an option.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else if (accountTypeSelection.getCheckedRadioButtonId() == R.id.toPatient){
+            setContentView(R.layout.patient_register_form);
+        }
+        else{
+            //TODO: Make it so this goes to the Doctor Registration Form
+        }
+
+    }
+
+    public void openSignIn(View view){
+        //setContentView(R.layout.signin_acount_type_selection_view);
+    }
+
 }
