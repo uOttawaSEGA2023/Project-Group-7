03f619a3f531087c0bd5690d8c5a6fc055162d54
SplitWolf <mat.jake1360@gmail.com>
2023-10-02 12:32:05 -0400

    docs: Add comments to fix rebase issue.

---
+++ app/src/main/java/com/seg2105a/projectgroup7/hams/user/LoginReturnCodes.java
@@ -0,0 +1,8 @@
+package com.seg2105a.projectgroup7.hams.user;
+
+public enum LoginReturnCodes {
+    //Types of login returns
+    Success,
+    UserDoesNotExist,
+    IncorrectPassword
+}
---
+++ app/src/main/java/com/seg2105a/projectgroup7/hams/user/UserType.java
@@ -0,0 +1,8 @@
+package com.seg2105a.projectgroup7.hams.user;
+
+public enum UserType {
+    //Types of users
+    DOCTOR,
+    PATIENT,
+    ADMIN
+}
