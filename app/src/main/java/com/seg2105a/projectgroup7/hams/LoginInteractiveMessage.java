849d0bd9fb3dcaa7cbd4a1aca0340317bb2b84a1
Anthony Le <theant741@gmail.com>
2023-10-03 21:30:55 -0400

    docs(UI): designed base structure of sign-in form; Added parts of interactive message java file.

--- app/src/main/java/com/seg2105a/projectgroup7/hams/LoginInteractiveMessage.java
+++ app/src/main/java/com/seg2105a/projectgroup7/hams/LoginInteractiveMessage.java
@@ -5,9 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.seg2105a.projectgroup7.hams.user.User;
 import com.seg2105a.projectgroup7.hams.user.UserType;
 
+import java.util.List;
+import java.util.Map;
+
 public class LoginInteractiveMessage extends AppCompatActivity {
     User tmpUser;
     UserType userType;
+    // goal for this file is to interact with both doctor and patient sign-up/sign-in activity.
+    // file outputs message identifying that they are logged in as a doctor, patient, or admin.
 
+
+    // potentially will have to import all activities; this could be a final output.
 
 }
