414b55024b44d84287c20bd34ab60aba581f8875
Anthony Le <theant741@gmail.com>
2023-10-03 20:18:27 -0400

    docs(UI): more minor changes in xml files; creation of interactive login page.

---
+++ app/src/main/java/com/seg2105a/projectgroup7/hams/LoginInteractiveMessage.java
@@ -0,0 +1,13 @@
+package com.seg2105a.projectgroup7.hams;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.seg2105a.projectgroup7.hams.user.User;
+import com.seg2105a.projectgroup7.hams.user.UserType;
+
+public class LoginInteractiveMessage extends AppCompatActivity {
+    User tmpUser;
+    UserType userType;
+
+
+}
