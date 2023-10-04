77d5958d38e81e184b9fc6cb92a03a3aea24877d
ARelaxedScholar <angemmanuel.kouakou+professional@gmail.com>
2023-10-04 10:37:24 -0400

    refactor, feat(Validator) : Added javadocs, and implemented basic phone number verification
    As I was thinking about writing validation for health card number, address and employee number, I realized it wasn't really feasible since too many formats exist. For address, using a specialized input would be smart, but for the rest we basically have to trust the user.

--- app/src/main/java/com/seg2105a/projectgroup7/hams/utils/Validator.java
+++ app/src/main/java/com/seg2105a/projectgroup7/hams/utils/Validator.java
@@ -9,13 +9,21 @@ import java.net.UnknownHostException;
 
 public class Validator {
 
+    /**
+     * Checks if the string that was passed by user is empty.
+     * @param stringField: The string which we want to verify
+     * @return {@code true} if yes, and {@code false} if it is NOT an empty string.
+     */
         public static boolean textFieldIsEmpty(String stringField){
             return (stringField == null || stringField.isEmpty());
         }
 
     /**
-     * @param emailAddress
-     * @return -1 if the domain is invalid, -2 if the localPart is invalid, 1 if everything is fine.
+     * Validates an email address by first checking if the domain associated with it exists, and then
+     * checking if the local part has a suitable format (checked through regex)
+     *
+     * @param emailAddress: The email address to validate
+     * @return {@code -1} if the domain is invalid, {@code -2} if the localPart is invalid, {@code 1} if everything is fine.
      */
         public static int emailAddressIsValid(@NonNull String emailAddress) {
             //Split the email address
@@ -34,8 +42,11 @@ public class Validator {
 
 
     /**
-     * @param name  : The name to validate
-     * @return true if the string is a name, false if not.
+     * Validates a name to ensure it matches the regex pattern that would verify most names
+     * (Sorry Elon's son, not yours)
+     *
+     * @param name: The name to validate
+     * @return {@code true} if the string does match the pattern, {@code false} if not.
      */
         public static boolean nameIsValid(@NonNull String name) {
             //Name is not valid, if contains non-alphabetic characters (sorry Elon's son)
@@ -46,25 +57,23 @@ public class Validator {
             return password.matches("^(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8}$\n");
         }
 
-        public static boolean phoneNumberIsValid(@NonNull String phoneNumber){
-            throw new IllegalStateException("Not yet implemented");
-        }
-
-        public static boolean postalAddressIsValid(@NonNull String postalAddress){
-            throw new IllegalStateException("Not yet implemented");
-        }
-
-        public static boolean healthCardNumberIsValid(@NonNull String healthCardNumber){
-            throw new IllegalStateException("Not yet implemented");
-        }
-
-        public static boolean employeeNumberIsValid(@NonNull String employeeNumber){
-            throw new IllegalStateException("Not yet implemented");
-        }
+    /**
+     * Validates a phone number to ensure it consists of exactly 10 digits (0-9).
+     *
+     * @param phoneNumber: The phone number to be validated.
+     * @return {@code true} if the phone number is valid (consists of exactly 10 digits), otherwise {@code false}.
+     * @throws NullPointerException if {@code phoneNumber} is {@code null}.
+     */
+    public static boolean phoneNumberIsValid(@NonNull String phoneNumber) {
+        return (phoneNumber.matches("[0-9]+") && phoneNumber.length() == 10);
+    }
 
     /**
-     * @param domain
-     * @return true if the domain is associated with an IP address, and false if not
+     * Validates a domain, by checking if there is an IP address associated with that hostname.
+     * Makes use of INetAddress.getByName method.
+     *
+     * @param domain: The domain to validate
+     * @return {@code true} if the domain is associated with an IP address, and {@code false} if not
      */
         private static boolean domainIsValid(String domain){
             try {
