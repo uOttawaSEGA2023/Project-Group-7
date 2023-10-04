b704c4ea2fbdd7c6f906ebc2d85d2da7e5d3d8a6
ARelaxedScholar <angemmanuel.kouakou+professional@gmail.com>
2023-10-04 10:55:47 -0400

    refactor, feat(Validator) : Added javadocs, and implemented basic phone number verification
    As I was thinking about writing validation for health card number, address and employee number, I realized it wasn't really feasible since too many formats exist. For address, using a specialized input would be smart, but for the rest we basically have to trust the user.

--- app/src/main/java/com/seg2105a/projectgroup7/hams/utils/Validator.java
+++ app/src/main/java/com/seg2105a/projectgroup7/hams/utils/Validator.java
@@ -3,20 +3,22 @@ package com.seg2105a.projectgroup7.hams.utils;
 import static java.net.InetAddress.getByName;
 
 
-
 import androidx.annotation.NonNull;
+
+import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class Validator {
 
     /**
      * Checks if the string that was passed by user is empty.
+     *
      * @param stringField: The string which we want to verify
      * @return {@code true} if yes, and {@code false} if it is NOT an empty string.
      */
-        public static boolean textFieldIsEmpty(String stringField){
-            return (stringField == null || stringField.isEmpty());
-        }
+    public static boolean textFieldIsEmpty(String stringField) {
+        return (stringField == null || stringField.isEmpty());
+    }
 
     /**
      * Validates an email address by first checking if the domain associated with it exists, and then
@@ -25,20 +27,20 @@ public class Validator {
      * @param emailAddress: The email address to validate
      * @return {@code -1} if the domain is invalid, {@code -2} if the localPart is invalid, {@code 1} if everything is fine.
      */
-        public static int emailAddressIsValid(@NonNull String emailAddress) {
-            //Split the email address
-            String[] splitEmail = emailAddress.split("@");
-            String localPart = splitEmail[0];
-            String domainPart = splitEmail[1];
+    public static int emailAddressIsValid(@NonNull String emailAddress) {
+        //Split the email address
+        String[] splitEmail = emailAddress.split("@");
+        String localPart = splitEmail[0];
+        String domainPart = splitEmail[1];
 
-            if (domainIsValid(domainPart)){
-                if(localPart.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$")){
-                    return 1;
-                }
-                return -2;
+        if (domainIsValid(domainPart)) {
+            if (localPart.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$")) {
+                return 1;
             }
-            return -1;
-         }
+            return -2;
+        }
+        return -1;
+    }
 
 
     /**
@@ -48,14 +50,14 @@ public class Validator {
      * @param name: The name to validate
      * @return {@code true} if the string does match the pattern, {@code false} if not.
      */
-        public static boolean nameIsValid(@NonNull String name) {
-            //Name is not valid, if contains non-alphabetic characters (sorry Elon's son)
-            return name.matches("^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$");
-        }
+    public static boolean nameIsValid(@NonNull String name) {
+        //Name is not valid, if contains non-alphabetic characters (sorry Elon's son)
+        return name.matches("^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$");
+    }
 
-        public static boolean passwordIsValid(@NonNull String password){
-            return password.matches("^(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8}$\n");
-        }
+    public static boolean passwordIsValid(@NonNull String password) {
+        return password.matches("^(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8}$\n");
+    }
 
     /**
      * Validates a phone number to ensure it consists of exactly 10 digits (0-9).
@@ -72,20 +74,20 @@ public class Validator {
      * Validates a domain, by checking if there is an IP address associated with that hostname.
      * Makes use of INetAddress.getByName method.
      *
-     * @param domain: The domain to validate
+     * @param domain : The domain to validate
      * @return {@code true} if the domain is associated with an IP address, and {@code false} if not
      */
-        private static boolean domainIsValid(String domain){
-            try {
-                getByName(domain);
+    private static boolean domainIsValid(String domain) {
+        try {
+            InetAddress address = getByName(domain);
 
-            } catch(UnknownHostException e) {
-                return false;
-            }
-            return true;
-
+        } catch (UnknownHostException e) {
+            return false;
         }
+        return true;
+
     }
+}
 
 
 
