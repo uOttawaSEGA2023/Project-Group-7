c6f9bb6230e9199c2c4870e3a7263c9542953851
ARelaxedScholar <angemmanuel.kouakou+professional@gmail.com>
2023-10-04 14:05:58 -0400

    feat(PatientSignUpActivity) : Added proper validation to all verifiable fields. (Names, Password, Email, Phone Number)
    This class still lacks javadocs, but this can be done later.

--- app/src/main/java/com/seg2105a/projectgroup7/hams/patient/activities/PatientSignUpActivity.java
+++ app/src/main/java/com/seg2105a/projectgroup7/hams/patient/activities/PatientSignUpActivity.java
@@ -1,6 +1,9 @@
 package com.seg2105a.projectgroup7.hams.patient.activities;
 
+import static com.seg2105a.projectgroup7.hams.utils.Validator.emailAddressIsValid;
 import static com.seg2105a.projectgroup7.hams.utils.Validator.nameIsValid;
+import static com.seg2105a.projectgroup7.hams.utils.Validator.passwordIsValid;
+import static com.seg2105a.projectgroup7.hams.utils.Validator.phoneNumberIsValid;
 import static com.seg2105a.projectgroup7.hams.utils.Validator.textFieldIsEmpty;
 
 import android.os.Bundle;
@@ -17,6 +20,7 @@ import com.seg2105a.projectgroup7.hams.patient.Patient;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class PatientSignUpActivity extends AppCompatActivity {
     private EditText firstNameEditText, lastNameEditText, emailAddressEditText, passwordEditText, phoneNumberEditText, postalAddressEditText, healthCardNumberEditText;
@@ -51,23 +55,56 @@ public class PatientSignUpActivity extends AppCompatActivity {
                 Toast.makeText(PatientSignUpActivity.this, "Please make sure to fill all the fields.", Toast.LENGTH_SHORT).show();
                 return;
             }
-            // Validate input (For now only name)
-            if (nameIsValid(firstName) && nameIsValid(lastName)) {
-                // Create a new user object
-                Patient newUser = new Patient(firstName, lastName, password.toCharArray(), emailAddress, phoneNumber, postalAddress, healthCardNumber);
+            //Validations Gauntlet
+            // Validate names
+            if (!(nameIsValid(firstName) && nameIsValid(lastName))) {
+                Toast.makeText(PatientSignUpActivity.this, "Please make sure your name follows a human format (no numbers, spaces, etc.)", Toast.LENGTH_LONG).show();
+                return;
+            }
+            //emailAddressIsValid throws errors due to be a wifi/threaded method, so we require a try/catch
+            try {
+                if (emailAddressIsValid(emailAddress) < 0) {
+                    if (emailAddressIsValid(emailAddress) == -1) {
+                        Toast.makeText(PatientSignUpActivity.this, "This email address is not formatted like an email address.", Toast.LENGTH_SHORT).show();
+                        return;
 
-                // Store the user data in the database
-                if (savedUser(newUser)) {
-                    // Registration successful
-                    Toast.makeText(PatientSignUpActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
-                    // Redirect to the login screen or do something else
-                } else {
-                    // Error while saving to the database
-                    Toast.makeText(PatientSignUpActivity.this, "Error occurred. Please try again.", Toast.LENGTH_SHORT).show();
+                    } else if (emailAddressIsValid(emailAddress) == -2) {
+                        Toast.makeText(PatientSignUpActivity.this, "Please ensure this email address' domain exists", Toast.LENGTH_SHORT).show();
+                        return;
+
+                    }
+                    Toast.makeText(PatientSignUpActivity.this, "Please ensure the localPart of your email address is correct, ensure there are no spaces.", Toast.LENGTH_SHORT).show();
+                    return;
                 }
+            }
+            catch(ExecutionException e){
+                Toast.makeText(PatientSignUpActivity.this, "Something went wrong during email's domain verification, please check your connection and try again.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            catch(InterruptedException e){
+                Toast.makeText(PatientSignUpActivity.this, "Something went wrong with the email address' verification thread, please wait a bit and try again.", Toast.LENGTH_SHORT).show();
+                return;
+
+            }
+            if (!passwordIsValid(password)){
+                Toast.makeText(PatientSignUpActivity.this, "Password must contain at least 8 chars, one capital letter and one small letter, one number, and one special character.", Toast.LENGTH_LONG).show();
+                return;
+            }
+            if (!phoneNumberIsValid(phoneNumber)){
+                Toast.makeText(PatientSignUpActivity.this, "Please make sure your phone number contains exactly 10 numbers, and only numbers.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            //If we haven't returned yet, it means the verifiable inputs have been verified. So we can attempt registration.
+            Patient newUser = new Patient(firstName, lastName, password.toCharArray(), emailAddress, phoneNumber, postalAddress, healthCardNumber);
+
+            // Store the user data in the database
+            if (savedUser(newUser)) {
+                // Registration successful
+                Toast.makeText(PatientSignUpActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
+                // Redirect to the login screen or do something else
             } else {
-                // Invalid input
-                Toast.makeText(PatientSignUpActivity.this, "Invalid input. Please check your data.", Toast.LENGTH_SHORT).show();
+                // Error while saving to the database
+                Toast.makeText(PatientSignUpActivity.this, "Error occurred. Please try again.", Toast.LENGTH_SHORT).show();
             }
         }
         );
