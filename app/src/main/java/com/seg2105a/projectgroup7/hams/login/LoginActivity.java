4c8e576c13a2e198c0926827aaf1ceae3c2a79c8
Anthony Le <theant741@gmail.com>
2023-10-06 08:38:44 -0400

    docs(UI): adding LoginActivity file

---
+++ app/src/main/java/com/seg2105a/projectgroup7/hams/login/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.seg2105a.projectgroup7.hams.login;
+
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.seg2105a.projectgroup7.hams.R;
+import com.seg2105a.projectgroup7.hams.user.User;
+
+import java.util.List;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailEditText, passwordEditText;
+    private Button signInButton;
+
+    private User loggedInUser;
+
+}
