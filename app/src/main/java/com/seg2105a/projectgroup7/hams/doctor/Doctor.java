45c624929e04e3b5971fa12ec6d94c96a20b5aee
SplitWolf <mat.jake1360@gmail.com>
2023-10-06 10:20:47 -0400

    refactor(doctor): Move specialties to its own file
    Rename private variables to follow conventions,
    Create Doctor Package

---
+++ app/src/main/java/com/seg2105a/projectgroup7/hams/doctor/Doctor.java
@@ -0,0 +1,44 @@
+package com.seg2105a.projectgroup7.hams.doctor;
+
+import com.seg2105a.projectgroup7.hams.user.User;
+
+import java.util.Set;
+
+public class Doctor extends User {
+	
+	private String _employeeNumber;
+	
+	private Set<Specialties> _specialties;
+	
+	
+	
+	public Doctor(String firstName, String lastName, byte[] hashedPassword, String email,
+			String phone, String address, String employeeNumber,Set<Specialties> specialties) {
+			
+			super(firstName, lastName, hashedPassword, email, phone, address);
+			
+			_employeeNumber = employeeNumber;
+			_specialties = specialties;
+	}
+
+
+	public String get_employeeNumber() {
+		return _employeeNumber;
+	
+	}
+	
+	public Set<Specialties> getSpecialties(){
+		return _specialties;
+	}
+
+	@Override
+	protected void signUp() {
+
+	}
+
+	@Override
+	protected void changeView() {
+
+	}
+			
+}
---
+++ app/src/main/java/com/seg2105a/projectgroup7/hams/doctor/Specialties.java
@@ -0,0 +1,10 @@
+package com.seg2105a.projectgroup7.hams.doctor;
+
+public enum Specialties {
+    FAMILY_MEDICINE,
+    INTERNAL_MEDICINE,
+    PEDIATRICS,
+    OBSTETRICS,
+    GYNECOLOGY
+
+}
