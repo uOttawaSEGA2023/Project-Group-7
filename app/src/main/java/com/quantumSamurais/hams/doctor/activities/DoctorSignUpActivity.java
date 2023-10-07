2c1a04fa9248e45b8a8b3c2a1e3d1368b334cfbd
SplitWolf <mat.jake1360@gmail.com>
2023-10-06 22:35:08 -0400

    feat(doctor): add Doctor SignUp Activity

---
+++ app/src/main/java/com/quantumSamurais/hams/doctor/activities/DoctorSignUpActivity.java
@@ -0,0 +1,14 @@
+package com.quantumSamurais.hams.doctor.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.quantumSamurais.hams.R;
+
+public class DoctorSignUpActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.doctor_register_form);
+    }
+}
