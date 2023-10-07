1796d9f975002970079b4b9802603c3dfc6a2df8
SplitWolf <mat.jake1360@gmail.com>
2023-10-06 22:37:46 -0400

    refactor(package): Change package name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "com.seg2105a.projectgroup7.hams"
+    namespace = "com.quantumSamurais.hams"
     compileSdk = 33
 
     defaultConfig {
@@ -36,6 +36,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-database")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
