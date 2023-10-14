ab3be9bc03be5b838e42cb977f0d0c57670ea803
SplitWolf <mat.jake1360@gmail.com>
2023-10-14 14:43:11 -0400

    feat(login): Updated to reference database instead of local maps. Added callback interface.

--- app/src/main/java/com/quantumSamurais/hams/login/Login.java
+++ app/src/main/java/com/quantumSamurais/hams/login/Login.java
@@ -2,13 +2,22 @@ package com.quantumSamurais.hams.login;
 
 //Firebase
 import android.content.Context;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 //App
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.firestore.Blob;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.protobuf.ByteString;
 import com.quantumSamurais.hams.admin.Administrator;
 import com.quantumSamurais.hams.doctor.Doctor;
 import com.quantumSamurais.hams.doctor.Specialties;
@@ -26,12 +35,16 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 
 public final class Login {
 
+    public interface LoginEventListener {
+        void loginResponse(LoginReturnCodes returnCode);
+    }
     private static FirebaseFirestore db = FirebaseFirestore.getInstance();
     /**
      *
@@ -41,19 +54,40 @@ public final class Login {
      * @param currentContext The current context so that we can switch the activity
      * @return Login codes that represent success or failure to login
      */
-    public static LoginReturnCodes login(String email, char[] password, UserType userType, Context currentContext) {
-        return  login(email,password,userType,currentContext,User.registeredPatients,User.registeredDoctors);
+    public static void login(String email, char[] password, UserType userType, Context currentContext, LoginEventListener loginEventListener) {
+        new Thread(() -> loginEventListener.loginResponse(getUserData(email,password,userType,currentContext))).start();
     };
 
-    private static User getUserData(UserType userType) {
-        //db code
-        dbcom L = new dbcom("123@gmail.com",userType);
-        while(!L.isDataReady()) {}
-        return L.getUser();
-
+    private static LoginReturnCodes getUserData(String email, char[] password, UserType userType, Context currentContext) {
+        QuerySnapshot snap = null;
+        switch (userType) {
+            case DOCTOR:
+                try {
+                    snap =  Tasks.await(db.collection("users").document("software").collection("doctors").get());
+                } catch (ExecutionException | InterruptedException ignored) {
+                }
+                return login(email,password,UserType.DOCTOR,currentContext,null,snap);
+            case PATIENT:
+                try {
+                    snap = Tasks.await(db.collection("users").document("software").collection("patients").get());
+                } catch (ExecutionException | InterruptedException ignored) {
+                }
+                return login(email,password,UserType.PATIENT,currentContext,snap,null);
+            case ADMIN:
+                return login(email,password,UserType.ADMIN,currentContext,null,null);
+        }
+        return LoginReturnCodes.UserDoesNotExist;
+    }
+    private static byte[] passwordFromDatabase(Map<String,Object> userData) {
+        Blob password = (Blob) userData.get("hashedPassword");
+        assert password != null;
+        byte[] pass = password.toBytes();
+        return pass;
     }
     private static byte[] saltFromDatabase(Map<String, Object> userData) {
-        return null;
+        Blob salt = (Blob) userData.get("salt");
+        assert salt != null;
+        return salt.toBytes();
     }
 
 
@@ -68,13 +102,16 @@ public final class Login {
      * @return Login codes that represent success or failure to login
      */
     private static LoginReturnCodes login(String email, char[] password, UserType userType, Context currentContext,
-                                         List<Map<String, Object>> patientList, List<Map<String, Object>> doctorList) {
+                                         QuerySnapshot patientList, QuerySnapshot doctorList) {
         Map<String, Object> userData = null;
         User loggedInUser = null;
         byte[] salt= null;
+;
         switch (userType) {
             case DOCTOR:
                 userData = searchLoop(doctorList,email);
+                if(userData == null)
+                    return LoginReturnCodes.UserDoesNotExist;
                 salt = saltFromDatabase(userData);
                 if(!Arrays.equals(hashPassword(password,salt), passwordFromDatabase(userData)))
                     return LoginReturnCodes.IncorrectPassword;
@@ -82,7 +119,8 @@ public final class Login {
                 loggedInUser = new Doctor(
                         (String)userData.get("firstName"),
                         (String) userData.get("lastName"),
-                        (byte[]) userData.get("password"),
+                        (byte[]) passwordFromDatabase(userData),
+                        (byte[]) saltFromDatabase(userData),
                         (String)userData.get("email"),
                         (String)userData.get("phone"),
                         (String)userData.get("address"),
@@ -98,10 +136,12 @@ public final class Login {
                 if(!Arrays.equals(hashPassword(password,salt), passwordFromDatabase(userData)))
                     return LoginReturnCodes.IncorrectPassword;
 
+
                 loggedInUser = new Patient(
                         (String)userData.get("firstName"),
                         (String) userData.get("lastName"),
-                        (byte[]) userData.get("password"),
+                        (byte[]) passwordFromDatabase(userData),
+                        (byte[]) saltFromDatabase(userData),
                         (String)userData.get("email"),
                         (String)userData.get("phone"),
                         (String)userData.get("address"),
@@ -122,9 +162,10 @@ public final class Login {
         loggedInUser.changeView(currentContext);
         return LoginReturnCodes.Success;
     }
-    private static Map<String, Object> searchLoop(List<Map<String, Object>> toSearch, String emailToSearch) {
-        for (Map<String, Object> userData : toSearch) {
-            if(!Objects.equals(userData.get("email"), emailToSearch)) continue;
+    private static Map<String, Object> searchLoop(QuerySnapshot toSearch, String emailToSearch) {
+        for (QueryDocumentSnapshot document : toSearch) {
+            Map<String, Object> userData = document.getData();
+            if(!Objects.equals(userData.get("emailAddress"), emailToSearch)) continue;
             return userData;
         }
         return null;
@@ -135,9 +176,6 @@ public final class Login {
      * @return The password hashed using PBKDF2WithHmacSHA1
      */
     public static byte[] hashPassword(char[] password, byte[] salt) {
-      //  byte[] salt = new byte[16];
-        SecureRandom random = new SecureRandom();
-        random.nextBytes(salt);
         KeySpec spec = new PBEKeySpec(password, salt, 65536, 128);
         try {
             SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
