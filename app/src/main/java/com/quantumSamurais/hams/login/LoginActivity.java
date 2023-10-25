4d40fce3d0165694164bd72ad8010488a8f482d0
ARelaxedScholar <angemmanuel.kouakou+professional@gmail.com>
2023-10-25 18:38:37 -0400

    style (LoginActivity.java) : Removed the giant comment that was there.

--- app/src/main/java/com/quantumSamurais/hams/login/LoginActivity.java
+++ app/src/main/java/com/quantumSamurais/hams/login/LoginActivity.java
@@ -65,43 +65,4 @@ public class LoginActivity extends AppCompatActivity implements LoginEventListen
         }
     }
 }
-//Original method that was implemented. Will need to go back to this if necessary.
-    /* public void onSignIn(View view) {
-        //getting user input from
-        signInButton.setOnClickListener(v-> {
 
-            String email = emailEditText.getText().toString().trim();
-            String password = passwordEditText.getText().toString().trim();
-
-            //we verify that the email and password is not empty.
-            if (textFieldIsEmpty(email) || textFieldIsEmpty(password)) {
-                Toast.makeText(LoginActivity.this, "Either the email or password fields is empty. Please make sure to fill out either or both fields.", Toast.LENGTH_LONG).show();
-
-            }
-
-            char[] parsePass = password.toCharArray();
-            Context context = this;
-            Boolean loggedIn = false; // kept as false for now; will find a boolean statement later.
-
-            UserType[] userArr = {UserType.PATIENT, UserType.ADMIN, UserType.DOCTOR};
-            for (int i = 0; i < userArr.length; i++) {
-                LoginReturnCodes tryLogin = Login.login(email, parsePass, userArr[i], LoginActivity.this);
-                if (tryLogin.equals(LoginReturnCodes.IncorrectPassword)) {
-                    Toast.makeText(LoginActivity.this, "Either the email or password fields is incorrect. Please try again.", Toast.LENGTH_LONG).show();
-                    break;
-                } else if (tryLogin.equals(LoginReturnCodes.Success)) {
-                    loggedIn = true;
-                    break;
-                }
-            }
-            if (loggedIn) {
-                Toast.makeText(context, "Successfully Logged in!", Toast.LENGTH_SHORT).show();
-                // Start the login activity
-                Intent intent = new Intent(context, LoginInteractiveMessage.class);
-                context.startActivity(intent);
-            }
-            // we connect this to Aryan's code.
-
-        });
-
-    }*/
