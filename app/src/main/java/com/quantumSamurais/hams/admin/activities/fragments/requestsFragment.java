31299c411cdcf543005bf972382c3ff4148edfbc
ARelaxedScholar <angemmanuel.kouakou+professional@gmail.com>
2023-10-27 16:21:23 -0400

    feat(requestsFragment.java) : The fragments that handle the requests info.

--- app/src/main/java/com/quantumSamurais/hams/admin/activities/fragments/requestsFragment.java
+++ app/src/main/java/com/quantumSamurais/hams/admin/activities/fragments/requestsFragment.java
@@ -52,7 +52,7 @@ public class requestsFragment extends Fragment implements RequestsActivityListen
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_all_requests, container, false);
+        View view = inflater.inflate(R.layout.fragment_requests, container, false);
         requestsStack = view.findViewById(R.id.requestsRecyclerView);
 
         Bundle args = getArguments();
