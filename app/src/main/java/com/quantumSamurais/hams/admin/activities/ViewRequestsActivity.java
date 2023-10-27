368628f0837c194fb2c642e874a44d2d5a98b863
ARelaxedScholar <angemmanuel.kouakou+professional@gmail.com>
2023-10-27 18:18:38 -0400

    refactor(ShowMoreActivity.java) : Moved the ShowMoreActivity.java into its own file.

--- app/src/main/java/com/quantumSamurais/hams/admin/activities/ViewRequestsActivity.java
+++ app/src/main/java/com/quantumSamurais/hams/admin/activities/ViewRequestsActivity.java
@@ -2,30 +2,22 @@ package com.quantumSamurais.hams.admin.activities;
 
 import static com.quantumSamurais.hams.admin.activities.fragments.requestsFragment.newInstance;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
 import com.quantumSamurais.hams.R;
 import com.quantumSamurais.hams.admin.adapters.RequestItemAdapter;
 import com.quantumSamurais.hams.admin.adapters.ViewPagerAdapter;
-import com.quantumSamurais.hams.database.DatabaseUtils;
-import com.quantumSamurais.hams.database.Request;
-
-import java.util.ArrayList;
 
 public class ViewRequestsActivity extends AppCompatActivity  {
-    DatabaseUtils tools = new DatabaseUtils();
+
     private TabLayout tabLayout;
     private ViewPager viewPager;
-    ArrayList<Request> requests;
-    RequestItemAdapter requestsAdapter;
-    RecyclerView requestsStack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,19 +42,7 @@ public class ViewRequestsActivity extends AppCompatActivity  {
 
 
 
-    public class ShowMoreActivity extends AppCompatActivity{
-        @Override
-        protected void onCreate(Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_show_more);
 
-            // Retrieve data passed
-            Intent intent = getIntent();
-            Request selectedRequest = intent.getParcelableExtra("selectedRequest");
-
-
-        }
-    }
 
 
 }
