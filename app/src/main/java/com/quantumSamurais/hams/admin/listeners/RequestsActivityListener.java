8b64d744f088cb731d264f4b37ac3e5098e1d9e1
ARelaxedScholar <angemmanuel.kouakou+professional@gmail.com>
2023-10-26 22:44:06 -0400

    feat(RequestActivityListener.java) : Added an intent to the onShowMoreClick() method so that info can be passed from RecyclerView.

--- app/src/main/java/com/quantumSamurais/hams/admin/listeners/RequestsActivityListener.java
+++ app/src/main/java/com/quantumSamurais/hams/admin/listeners/RequestsActivityListener.java
@@ -1,9 +1,10 @@
 package com.quantumSamurais.hams.admin.listeners;
 
+import android.content.Intent;
 import android.view.View;
 
 public interface RequestsActivityListener {
     void onAcceptClick(int position);
     void onRejectClick(int position);
-    void onShowMoreClick(int position);
+    void onShowMoreClick(int position, Intent showMore);
 }
