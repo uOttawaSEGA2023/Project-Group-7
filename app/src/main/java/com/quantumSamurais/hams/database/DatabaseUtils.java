d53ad482c3cce4160421d55691aad590dd5094f4
SplitWolf <mat.jake1360@gmail.com>
2023-10-28 00:30:01 -0400

    fix(imports): Fixed imports

--- app/src/main/java/com/quantumSamurais/hams/database/DatabaseUtils.java
+++ app/src/main/java/com/quantumSamurais/hams/database/DatabaseUtils.java
@@ -1,6 +1,8 @@
 package com.quantumSamurais.hams.database;
 
+import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -10,14 +12,24 @@ import com.google.firebase.firestore.QuerySnapshot;
 import com.quantumSamurais.hams.database.callbacks.ResponseListener;
 import com.quantumSamurais.hams.doctor.Doctor;
 import com.quantumSamurais.hams.patient.Patient;
+import com.quantumSamurais.hams.user.User;
 import com.quantumSamurais.hams.user.UserType;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 public class DatabaseUtils {
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
     public void addSignUpRequest(Patient user) {
         new Thread(() -> {
                 try {
@@ -200,7 +212,7 @@ public class DatabaseUtils {
         new Thread(()->{
             listener.onSuccess(getStatus(email,userType));
         }).start();
-    };
+    }
 
 
     private RequestStatus getStatus(String email, UserType userType) {
@@ -264,58 +276,58 @@ public class DatabaseUtils {
      *
      * @param */
 
-//    public static void sendEmail(Context context, User user, RequestStatus status) {
-//        try {
-//            new Thread(() -> {
-//                // should be put into an on click for both the approve and reject buttons.
-//                final String username = "admin@gmail.com"; // template admin email for the time being.
-//                final String password = "12345678"; // '' password.
-//                String msgToSend = new String(), msgSbjct = new String(); // message to send and message subject.
-//                switch (status) {
-//                    case APPROVED:
-//                        msgToSend = "Your request to the health Management app has been approved.\n";
-//                        msgSbjct = "your account has been approved! :D";
-//                        break;
-//                    case REJECTED:
-//                        msgToSend = "Your request to the health Management app has been denied. \n ";
-//                        msgSbjct = "your account has been denied. >:0 ";
-//                        break;
-//                }
-//                Properties props = new Properties();
-//                props.put("mail.smtp.auth", "true");
-//                props.put("mail.smtp.starttls.enable", "true");
-//                props.put("mail.smtp.host", "smtp.gmail.com"); // set as smtp.gmail.com because of email domain.
-//                // change if we the email has another domain.
-//                props.put("mail.smtp.port", "587"); // 587 most common port for sending emails.
-//
-//
-//                Session session = Session.getInstance(props,
-//                        new javax.mail.Authenticator() {
-//                            @Override
-//                            protected PasswordAuthentication getPasswordAuthentication() {
-//                                return new PasswordAuthentication(username, password);
-//                            }
-//                        });
-//
-//                try { // try block for sending the email.
-//                    Message message = new MimeMessage(session);
-//                    message.setFrom(new InternetAddress(username));
-//                    message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(user.getEmail()));
-//                    // we set recipients of the email as the user that we verify or reject.
-//                    message.setSubject(msgSbjct);
-//                    message.setText(msgToSend);
-//                    Transport.send(message);
-//                    Toast.makeText(context, "patient has been notified.", Toast.LENGTH_LONG).show();
-//
-//
-//                } catch (MessagingException e) {
-//                    throw new RuntimeException(e);
-//                }
-//            }).start();
-//        }
-//        catch (Exception e){
-//            Log.d("View ", "So that happened: " + e.getStackTrace());
-//        }
-//
-//    }
+    public static void sendEmail(Context context, User user, RequestStatus status) {
+        try {
+            new Thread(() -> {
+                // should be put into an on click for both the approve and reject buttons.
+                final String username = "admin@gmail.com"; // template admin email for the time being.
+                final String password = "12345678"; // '' password.
+                String msgToSend = "", msgSbjct = ""; // message to send and message subject.
+                switch (status) {
+                    case APPROVED:
+                        msgToSend = "Your request to the health Management app has been approved.\n";
+                        msgSbjct = "your account has been approved! :D";
+                        break;
+                    case REJECTED:
+                        msgToSend = "Your request to the health Management app has been denied. \n ";
+                        msgSbjct = "your account has been denied. >:0 ";
+                        break;
+                }
+                Properties props = new Properties();
+                props.put("mail.smtp.auth", "true");
+                props.put("mail.smtp.starttls.enable", "true");
+                props.put("mail.smtp.host", "smtp.gmail.com"); // set as smtp.gmail.com because of email domain.
+                // change if we the email has another domain.
+                props.put("mail.smtp.port", "587"); // 587 most common port for sending emails.
+
+
+                Session session = Session.getInstance(props,
+                        new javax.mail.Authenticator() {
+                            @Override
+                            protected PasswordAuthentication getPasswordAuthentication() {
+                                return new PasswordAuthentication(username, password);
+                            }
+                        });
+
+                try { // try block for sending the email.
+                    Message message = new MimeMessage(session);
+                    message.setFrom(new InternetAddress(username));
+                    message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(user.getEmail()));
+                    // we set recipients of the email as the user that we verify or reject.
+                    message.setSubject(msgSbjct);
+                    message.setText(msgToSend);
+                    Transport.send(message);
+                    Toast.makeText(context, "patient has been notified.", Toast.LENGTH_LONG).show();
+
+
+                } catch (MessagingException e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
+        }
+        catch (Exception e){
+            Log.d("View ", "So that happened: " + e.getStackTrace());
+        }
+
+    }
 }
