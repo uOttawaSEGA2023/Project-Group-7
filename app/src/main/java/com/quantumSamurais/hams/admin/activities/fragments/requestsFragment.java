b51911a2cfa6409a4b2eec4ed044342ad3ff71a3
ARelaxedScholar <angemmanuel.kouakou+professional@gmail.com>
2023-10-28 01:23:34 -0400

    feat(requestsFragment): Make the different requests show to the recycler view.

--- app/src/main/java/com/quantumSamurais/hams/admin/activities/fragments/requestsFragment.java
+++ app/src/main/java/com/quantumSamurais/hams/admin/activities/fragments/requestsFragment.java
@@ -1,8 +1,7 @@
 package com.quantumSamurais.hams.admin.activities.fragments;
 
-import static com.quantumSamurais.hams.database.DatabaseUtils.sendEmail;
-import static com.quantumSamurais.hams.database.RequestStatus.APPROVED;
-import static com.quantumSamurais.hams.database.RequestStatus.REJECTED;
+import static com.quantumSamurais.hams.user.UserType.DOCTOR;
+import static com.quantumSamurais.hams.user.UserType.PATIENT;
 
 import android.content.Context;
 import android.content.Intent;
@@ -19,12 +18,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.quantumSamurais.hams.R;
+import com.quantumSamurais.hams.admin.activities.ShowMoreActivity;
 import com.quantumSamurais.hams.admin.adapters.RequestItemAdapter;
 import com.quantumSamurais.hams.admin.adapters.RequestItemAdapter.FragmentTab;
 import com.quantumSamurais.hams.admin.listeners.RequestsActivityListener;
 import com.quantumSamurais.hams.database.DatabaseUtils;
 import com.quantumSamurais.hams.database.Request;
 import com.quantumSamurais.hams.database.callbacks.RequestsResponseListener;
+import com.quantumSamurais.hams.doctor.Doctor;
+import com.quantumSamurais.hams.patient.Patient;
 import com.quantumSamurais.hams.user.User;
 
 import java.util.ArrayList;
@@ -53,14 +55,16 @@ public class requestsFragment extends Fragment implements RequestsActivityListen
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_requests, container, false);
         requestsStack = view.findViewById(R.id.requestsRecyclerViewFragment);
+        tools = new DatabaseUtils();
+        requests = new ArrayList<>();
 
         Bundle args = getArguments();
         if (args != null) {
             activeTab = (FragmentTab) args.getSerializable("activeTab");
             refreshHandler.post(refreshRunnable);
             Log.d("requests Fragment", "Instance requests size after refreshRunnable : " + requests.size());
-            requestsAdapter = new RequestItemAdapter(getContext(), activeTab, requests, this);
-            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+            requestsAdapter = new RequestItemAdapter(getActivity(), activeTab, requests, this);
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
             requestsStack.setLayoutManager(layoutManager);
             requestsStack.setAdapter(requestsAdapter);
 
@@ -74,8 +78,6 @@ public class requestsFragment extends Fragment implements RequestsActivityListen
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        tools = new DatabaseUtils();
-
     }
 
     @Override
@@ -88,9 +90,6 @@ public class requestsFragment extends Fragment implements RequestsActivityListen
         tools.getSignUpRequests(this);
     }
 
-    public void RefreshButtonClicked(View v){
-        viewRegistrationRequests();
-    }
 
     // This handler is needed to allow automatic refresh of the screen
     private Handler refreshHandler = new Handler(Looper.getMainLooper());
@@ -99,22 +98,22 @@ public class requestsFragment extends Fragment implements RequestsActivityListen
         @Override
         public void run() {
             viewRegistrationRequests();
-            refreshHandler.postDelayed(this, 5000);
         }
     };
     @Override
-    public void onSuccess(ArrayList<Request> requests) {
+    public void onSuccess(ArrayList<Request> requestsFromDatabase) {
+        Log.d("RequestFragment", "Number of items in requests prechange: " + requests.size());
+        requests = requestsFromDatabase;
+        Log.d("RequestFragmentX", "Number of items in requests after change: " + requests.size());
 
-        this.requests = requests;
-        getActivity().runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                Log.d("RequestFragment", "Number of items in requests: " + requests.size());
-                requestsAdapter.notifyDataSetChanged();
-            }
+        getActivity().runOnUiThread(() -> {
+            requestsAdapter.setRequests(requests);
         });
-        // Run the post again after 5 seconds
+        //Clear the Handler Queue
+        stopDataRefresh();
+        //
         refreshHandler.postDelayed(refreshRunnable, 5000);
+
     }
     private void stopDataRefresh() {
         refreshHandler.removeCallbacks(refreshRunnable);
@@ -128,19 +127,21 @@ public class requestsFragment extends Fragment implements RequestsActivityListen
 
     @Override
     public void onAcceptClick(int position) {
+        stopDataRefresh(); //I do not want the requests to be updated as I am accessing it.
         Log.d("requests Fragment", "accept click was pressed");
         long idToAccept = requests.get(position).getID();
         tools.approveSignUpRequest(idToAccept);
-        sendEmail(getActivity(), getUserFromRequest(requests.get(position)), APPROVED);
+        //sendEmail(getActivity(), getUserFromRequest(requests.get(position)), APPROVED);
         refreshHandler.post(refreshRunnable);
     }
 
     @Override
     public void onRejectClick(int position) {
+        stopDataRefresh();
         Log.d("requests Fragment", "reject click was pressed");
         long idToReject = requests.get(position).getID();
-        tools.approveSignUpRequest(idToReject);
-        sendEmail(getActivity(), getUserFromRequest(requests.get(position)), REJECTED);
+        tools.rejectSignUpRequest(idToReject);
+        //sendEmail(getActivity(), getUserFromRequest(requests.get(position)), REJECTED);
         refreshHandler.post(refreshRunnable);
     }
 
@@ -161,19 +162,36 @@ public class requestsFragment extends Fragment implements RequestsActivityListen
     }
 
     @Override
-    public void onShowMoreClick(int position, Intent showMore) {
+    public void onShowMoreClick(int position) {
+        //Get the selected request from the list
+        Request selectedRequest = requests.get(position);
+        Intent showMoreIntent = new Intent(getActivity(), ShowMoreActivity.class);
+        switch (selectedRequest.getUserType()){
+            case DOCTOR:
+                Doctor someDoctor = (Doctor) getUserFromRequest(selectedRequest);
+                showMoreIntent.putExtra("userType", DOCTOR);
+                showMoreIntent.putExtra("firstName", someDoctor.getFirstName());
+                showMoreIntent.putExtra("lastName", someDoctor.getLastName());
+                showMoreIntent.putExtra("email", someDoctor.getEmail());
+                showMoreIntent.putExtra("phoneNumber", someDoctor.getPhone());
+                showMoreIntent.putExtra("address", someDoctor.getAddress());
+                showMoreIntent.putExtra("employeeNumber", someDoctor.getEmployeeNumber());
+                showMoreIntent.putExtra("specialties", someDoctor.getSpecialties()) ;
+                break;
+            case PATIENT:
+                Patient somePatient = (Patient) getUserFromRequest(selectedRequest);
+                showMoreIntent.putExtra("userType", PATIENT);
+                showMoreIntent.putExtra("firstName", somePatient.getFirstName());
+                showMoreIntent.putExtra("lastName", somePatient.getLastName());
+                showMoreIntent.putExtra("email", somePatient.getEmail());
+                showMoreIntent.putExtra("phoneNumber", somePatient.getPhone());
+                showMoreIntent.putExtra("address", somePatient.getAddress());
+                showMoreIntent.putExtra("healthCardNumber", somePatient.getHealthCardNumber());
+                break;
+            case ADMIN:
+                //Shouldn't happen
+        }
 
-        // Get the selected request from the list
-        //Request selectedRequest = requests.get(position);
-
-        // Create an Intent to navigate to the "Show More" page
-        //Intent showMoreIntent = new Intent(this, ShowMoreActivity.class);
-
-        // Pass the selected request's data to the "Show More" page
-        //showMoreIntent.putExtra("selectedRequest", selectedRequest);
-
-
-        // Start the "Show More" activity
-        //startActivity(showMoreIntent);
+        startActivity(showMoreIntent);
     }
 }
--- app/src/main/java/com/quantumSamurais/hams/admin/listeners/RequestsActivityListener.java
+++ app/src/main/java/com/quantumSamurais/hams/admin/listeners/RequestsActivityListener.java
@@ -1,10 +1,7 @@
 package com.quantumSamurais.hams.admin.listeners;
 
-import android.content.Intent;
-import android.view.View;
-
 public interface RequestsActivityListener {
     void onAcceptClick(int position);
     void onRejectClick(int position);
-    void onShowMoreClick(int position, Intent showMore);
+    void onShowMoreClick(int position);
 }
