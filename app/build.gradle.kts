ed409d05aa6b4cae358ef96aa0d8a948d5d10447
ayprusss <theant741@gmail.com>
2023-11-13 23:36:22 -0500

    feat(navigationDrawer): basic implementation for navigation drawer and fragments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.google.firebase:firebase-firestore")
+    implementation("androidx.navigation:navigation-fragment:2.7.5")
+    implementation("androidx.navigation:navigation-ui:2.7.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/quantumSamurais/hams/settings/settingfragment.java
@@ -0,0 +1,4 @@
+package com.quantumSamurais.hams.settings;
+
+public class settingfragment {
+}
