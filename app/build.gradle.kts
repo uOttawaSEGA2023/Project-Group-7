2ae346135e3391ace9d3883398e9010ce29303a7
ayprusss <theant741@gmail.com>
2023-11-15 18:56:18 -0500

    implemented more specifications for gradle build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,5 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.sun.mail:android-mail:1.6.7")
     implementation("com.sun.mail:android-activation:1.6.7")
+    implementation("com.google.android.material:material:1.3.0-alpha03")
 }
