517a72fbef62aa46561f5a9538dcf83b8ab1209a
ARelaxedScholar <angemmanuel.kouakou+professional@gmail.com>
2023-11-15 12:34:13 -0500

    feat(DoctorMain): Added the refreshHandler stuff.

--- app/src/main/java/com/quantumSamurais/hams/doctor/activities/DoctorMain.java
+++ app/src/main/java/com/quantumSamurais/hams/doctor/activities/DoctorMain.java
@@ -1,6 +1,7 @@
 package com.quantumSamurais.hams.doctor.activities;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,28 +24,41 @@ public class DoctorMain extends AppCompatActivity implements DoctorShiftsAdapter
     RecyclerView shiftsStack;
     Database db;
     ArrayList<Shift> shifts;
+    Handler refreshShifts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         if (getIntent().getStringExtra("doctorEmailAddress") == null){
             Log.d("doctorMain", "somehow the intent wasn't passed properly.");
-            /*Toast.makeText(this, "An error occurred, this session will be terminated, try again", Toast.LENGTH_SHORT).show();
-            finish();*/
+            Toast.makeText(this, "An error occurred, this session will be terminated, try again", Toast.LENGTH_SHORT).show();
+            finish();
         }
 
         db = Database.getInstance();
         myDoctor = db.getDoctor(getIntent().getStringExtra("doctorEmailAddress"));
         shifts = myDoctor.getShifts();
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.main_doctor_view);
         setup();
+        refreshShifts = new Handler();
+        refresh_runnable.run();
     }
+
+    private final Runnable refresh_runnable = new Runnable(){
+        public void run(){
+            Log.d("Doctor", "My doctor to string: " + myDoctor.toString());
+            Log.d("Doctor", "My shifts: " + shifts);
+            myDoctor = db.getDoctor(myDoctor.getEmail());
+            shiftsAdapter.updateList(myDoctor.getShifts());
+            shiftsAdapter.notifyDataSetChanged();
+            refreshShifts.postDelayed(refresh_runnable, 5000);
+        }
+    };
 
     private void setup(){
         //Sets the info for the Tab Layout
         TextView firstName = findViewById(R.id.firstNameDoc);
         firstName.setText(myDoctor.getFirstName());
         Log.d("doctor", "This is my name: " + myDoctor.toString());
-
         //Setting up the RecyclerView for Shifts
         shiftsAdapter = new DoctorShiftsAdapter(shifts, this);
         shiftsStack = findViewById(R.id.shiftsRecyclerView);
@@ -57,15 +71,32 @@ public class DoctorMain extends AppCompatActivity implements DoctorShiftsAdapter
     public void onDeleteClick(int position) {
         long shiftID = shifts.get(position).getShiftID();
         myDoctor.cancelShift(shiftID);
+        // Remove first to avoid, any potential slurry of callbacks problem
+        refreshShifts.removeCallbacks(refresh_runnable);
+        // Then we update
+        refreshShifts.post(refresh_runnable);
     }
 
     @Override
     public void onResume() {
         super.onResume();
         //Updates the doctor
-        myDoctor = db.getDoctor(myDoctor.getEmail());
+        refreshShifts.post(refresh_runnable);
     }
 
-    //Refresh Handler to refresh the UI
+    @Override
+    protected void onPause() {
+        super.onPause();
+        refreshShifts.removeCallbacks(refresh_runnable);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        finish();
+    }
+
+
+
 
 }
