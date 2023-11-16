a1bde7e3f8700cd42f0a1043e07cdc9b31c8e144
ARelaxedScholar <angemmanuel.kouakou+professional@gmail.com>
2023-11-15 22:22:54 -0500

    feat(DoctorViewAppointments): It's now a thing and should work as it's based on the Admin requests.

--- app/src/main/java/com/quantumSamurais/hams/doctor/activities/DoctorViewAppointments.java
+++ app/src/main/java/com/quantumSamurais/hams/doctor/activities/DoctorViewAppointments.java
@@ -1,7 +1,44 @@
 package com.quantumSamurais.hams.doctor.activities;
 
+import static com.quantumSamurais.hams.admin.activities.fragments.requestsFragment.newInstance;
+import static com.quantumSamurais.hams.core.enums.FragmentTab.ALL_REQUESTS;
+import static com.quantumSamurais.hams.core.enums.FragmentTab.PENDING_REQUESTS;
+import static com.quantumSamurais.hams.core.enums.FragmentTab.REJECTED_REQUESTS;
+
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+import com.quantumSamurais.hams.R;
+import com.quantumSamurais.hams.core.adapters.ViewPagerAdapter;
 
 public class DoctorViewAppointments extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.see_appointments);
+        setup();
+    }
+
+    public void setup() {
+        tabLayout = findViewById(R.id.appointmentsTabs);
+        viewPager = findViewById(R.id.tabsViewPager4Appointments);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        //Sets the info for the Tab Layout
+        ViewPagerAdapter vpAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragments(newInstance(ALL_REQUESTS), "All Requests");
+        vpAdapter.addFragments(newInstance(PENDING_REQUESTS), "Pending");
+        vpAdapter.addFragments(newInstance(REJECTED_REQUESTS), "Rejected");
+        viewPager.setAdapter(vpAdapter);
+    }
+
 
 }
