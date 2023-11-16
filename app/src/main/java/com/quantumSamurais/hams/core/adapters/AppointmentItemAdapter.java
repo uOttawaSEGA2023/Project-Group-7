4162f8acf6deac7da881b91cee9cdc42749e1ff0
ARelaxedScholar <angemmanuel.kouakou+professional@gmail.com>
2023-11-15 21:43:29 -0500

    feat(Appointment): Big commit, contains the essentials for the appointment handling. DoctorViewAppointments will be connected to DoctorMain.

---
+++ app/src/main/java/com/quantumSamurais/hams/core/adapters/AppointmentItemAdapter.java
@@ -0,0 +1,206 @@
+package com.quantumSamurais.hams.core.adapters;
+
+import static com.quantumSamurais.hams.database.RequestStatus.PENDING;
+import static com.quantumSamurais.hams.database.RequestStatus.REJECTED;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.quantumSamurais.hams.R;
+import com.quantumSamurais.hams.admin.Administrator;
+import com.quantumSamurais.hams.appointment.Appointment;
+import com.quantumSamurais.hams.core.listeners.RequestsActivityListener;
+import com.quantumSamurais.hams.database.Database;
+import com.quantumSamurais.hams.doctor.Doctor;
+import com.quantumSamurais.hams.patient.Patient;
+import com.quantumSamurais.hams.user.User;
+
+import java.util.ArrayList;
+
+
+
+
+public class AppointmentItemAdapter extends RecyclerView.Adapter<AppointmentItemAdapter.RequestViewHolder> {
+    private final RequestsActivityListener requestClickListener;
+    private ArrayList<Appointment> appointments; // Renamed from 'requests'
+    private final Context currentContext;
+    FragmentTab activeTab;
+    Database db = Database.getInstance();
+
+    public enum FragmentTab {
+        ALL_REQUESTS,
+        PENDING_REQUESTS,
+        REJECTED_REQUESTS
+    }
+
+    public AppointmentItemAdapter(Context context, FragmentTab activeTab, ArrayList<Appointment> appointmentsFromDatabase, RequestsActivityListener listener) {
+        Log.d("AppointmentItemAdapter", "Number of items in appointments: " + appointmentsFromDatabase.size());
+        this.activeTab = activeTab;
+        setAppointments(appointmentsFromDatabase);
+        currentContext = context;
+        requestClickListener = listener;
+        Log.d("AppointmentItemAdapter", "Number of items in appointments: " + appointments.size());
+    }
+
+
+    public void setAppointments(ArrayList<Appointment> appointmentsFromDatabase){
+        // Filters the passed list, and makes it so it contains only the required info
+        ArrayList<Appointment> tempAppointments = appointmentsFromDatabase;
+        switch(activeTab){
+            case ALL_REQUESTS:
+                break;
+            case PENDING_REQUESTS:
+                ArrayList<Appointment> pendingAppointments = new ArrayList<>();
+                for (Appointment appointment : appointmentsFromDatabase){
+                    if (appointment.getAppointmentStatus() == PENDING){
+                        pendingAppointments.add(appointment);
+                    }
+                }
+                tempAppointments = pendingAppointments;
+                break;
+
+            case REJECTED_REQUESTS:
+                ArrayList<Appointment> rejectedAppointments = new ArrayList<>();
+                for (Appointment appointment : appointmentsFromDatabase){
+                    if (appointment.getAppointmentStatus() == REJECTED){
+                        rejectedAppointments.add(appointment);
+                    }
+                }
+                tempAppointments  = rejectedAppointments;
+                break;
+        }
+        appointments = tempAppointments ;
+        notifyDataSetChanged();
+    }
+
+    public static class RequestViewHolder extends RecyclerView.ViewHolder{
+        TextView name, emailAddress, userType, requestId;
+        ImageButton accept, reject, moreInfo;
+        Appointment appointment;
+        RequestsActivityListener requestsActivityListener;
+
+        public RequestViewHolder(@NonNull View itemView, RequestsActivityListener requestClickListener) {
+            super(itemView);
+            requestsActivityListener = requestClickListener;
+            accept = itemView.findViewById(R.id.accept);
+            reject = itemView.findViewById(R.id.deny);
+            moreInfo = itemView.findViewById(R.id.showMore);
+        }
+
+        private void setData(User user, long id) throws Exception {
+            name = itemView.findViewById(R.id.nameRequest);
+            emailAddress = itemView.findViewById(R.id.emailAddressRequest);
+            userType = itemView.findViewById(R.id.userTypeRequest);
+            requestId = itemView.findViewById(R.id.idRequest);
+
+            //
+            name.setText(user.getFirstName() + " " + user.getLastName());
+            emailAddress.setText(user.getEmail());
+            requestId.setText(Long.toString(id));
+
+            //Since it's an ENUM, default is unneeded.
+            if (user instanceof Doctor) {
+                userType.setText(R.string.doctor);
+            } else if (user instanceof Patient) {
+                userType.setText(R.string.patient);
+            } else if (user instanceof Administrator) {
+                Log.d("Request Screen", "Someone managed to create an account as ADMIN... how?");
+            }
+            setOnClickListeners(appointment);
+
+
+
+
+        }
+
+        public void setAppointment(Appointment appointment) {
+            this.appointment = appointment;
+        }
+        private void setOnClickListeners(Appointment appointment){
+            View.OnClickListener acceptListener = view -> {
+                if (requestsActivityListener != null) {
+                    int position = getAdapterPosition();
+
+                    if (position != RecyclerView.NO_POSITION) {
+                        requestsActivityListener.onAcceptClick(position);
+                    }
+
+                }
+            };
+            View.OnClickListener rejectListener = view -> {
+                if (requestsActivityListener != null) {
+                    int position = getAdapterPosition();
+
+                    if (position != RecyclerView.NO_POSITION) {
+                        requestsActivityListener.onRejectClick(position);
+                    }
+
+                }
+            };
+            View.OnClickListener showMoreListener = view -> {
+                if (requestsActivityListener != null) {
+                    int position = getAdapterPosition();
+                    Intent intent = new Intent();
+
+                    if (position != RecyclerView.NO_POSITION) {
+                        requestsActivityListener.onShowMoreClick(position);
+                    }
+
+                }
+            };
+            //Always there.
+            accept.setOnClickListener(acceptListener);
+            moreInfo.setOnClickListener(showMoreListener);
+
+
+
+            //It only makes sense to have X button for requests that are pending.
+            if (appointment.getAppointmentStatus() == REJECTED) {
+                reject.setVisibility(View.INVISIBLE); // Hide the button
+                reject.setEnabled(false); // Make the button uninteractable
+            } else {
+                //Since the view might be recycled from an object which was REJECTED
+                //To make sure the reject button is accessible we have to set it the onclick
+                reject.setVisibility(View.VISIBLE);
+                reject.setEnabled(true);
+                reject.setOnClickListener(rejectListener);
+
+
+            }
+        }
+
+    }
+
+    @NonNull
+    @Override
+    public AppointmentItemAdapter.RequestViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(currentContext).inflate(R.layout.patient_appointment, parent,false);
+        return new RequestViewHolder(v, requestClickListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AppointmentItemAdapter.RequestViewHolder holder, int position) {
+        try {
+            long appointmentID = appointments.get(position).getAppointmentID();
+            holder.setAppointment(appointments.get(position));
+            holder.setData(db.getPatientFromAppointmentID(appointmentID), appointmentID);
+        } catch (Exception e) {
+            Log.d("Requests Screen", e.getMessage() + " " + e.getCause());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return appointments.size();
+    }
+
+}
---
+++ app/src/main/java/com/quantumSamurais/hams/doctor/activities/fragments/appointmentsFragment.java
@@ -0,0 +1,177 @@
+package com.quantumSamurais.hams.doctor.activities.fragments;
+
+import static com.quantumSamurais.hams.user.UserType.PATIENT;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.quantumSamurais.hams.R;
+import com.quantumSamurais.hams.admin.activities.ShowMoreActivity;
+import com.quantumSamurais.hams.appointment.Appointment;
+import com.quantumSamurais.hams.core.adapters.AppointmentItemAdapter;
+import com.quantumSamurais.hams.core.adapters.AppointmentItemAdapter.FragmentTab;
+import com.quantumSamurais.hams.core.listeners.RequestsActivityListener;
+import com.quantumSamurais.hams.database.Database;
+import com.quantumSamurais.hams.database.callbacks.ResponseListener;
+import com.quantumSamurais.hams.doctor.Doctor;
+import com.quantumSamurais.hams.patient.Patient;
+
+import java.util.ArrayList;
+
+
+public class appointmentsFragment extends Fragment implements RequestsActivityListener, ResponseListener<ArrayList<Appointment>> {
+    FragmentTab activeTab;
+    AppointmentItemAdapter requestsAdapter;
+    RecyclerView requestsStack;
+    Database db;
+    ArrayList<Appointment> appointments;
+    Doctor myDoctor;
+
+    public appointmentsFragment() {
+        // Required empty public constructor
+    }
+
+    public static appointmentsFragment newInstance(FragmentTab activeTab, Doctor doctor) {
+        appointmentsFragment fragment = new appointmentsFragment();
+        fragment.setMyDoctor(doctor);
+        Bundle args = new Bundle();
+        args.putSerializable("activeTab", activeTab);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_requests, container, false);
+        requestsStack = view.findViewById(R.id.requestsRecyclerViewFragment);
+        db = Database.getInstance();
+        appointments = new ArrayList<>();
+
+        Bundle args = getArguments();
+        if (args != null) {
+            activeTab = (FragmentTab) args.getSerializable("activeTab");
+            refreshHandler.post(refreshRunnable);
+            Log.d("requests Fragment", "Instance requests size after refreshRunnable : " + appointments.size());
+            requestsAdapter = new AppointmentItemAdapter(getActivity(), activeTab, appointments, this);
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+            requestsStack.setLayoutManager(layoutManager);
+            requestsStack.setAdapter(requestsAdapter);
+
+            Log.d("requests Fragment", "We called from onCreateView");
+            return view;
+        }
+        throw new IllegalStateException("This fragment was generated without using the newInstance method and hence has no FragmentTab.");
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        viewAppointments();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        stopDataRefresh();
+    }
+
+    public void setMyDoctor(Doctor someDoctor){myDoctor = someDoctor;}
+
+    public void viewAppointments() {
+        db.getDoctorAppointments(null);
+    }
+
+
+    // This handler is needed to allow automatic refresh of the screen
+    private final Handler refreshHandler = new Handler(Looper.getMainLooper());
+
+    private final Runnable refreshRunnable = new Runnable() {
+        @Override
+        public void run() {
+            viewAppointments();
+        }
+    };
+
+    @Override
+    public void onSuccess(ArrayList<Appointment> appointmentsFromDatabase) {
+        Log.d("RequestFragment", "Number of items in requests prechange: " + appointments.size());
+        appointments = appointmentsFromDatabase;
+        Log.d("RequestFragmentX", "Number of items in requests after change: " + appointments.size());
+
+        getActivity().runOnUiThread(() -> {
+            requestsAdapter.setAppointments(appointments);
+        });
+        //Clear the Handler Queue
+        stopDataRefresh();
+        //
+        refreshHandler.postDelayed(refreshRunnable, 5000);
+
+    }
+
+    private void stopDataRefresh() {
+        refreshHandler.removeCallbacks(refreshRunnable);
+    }
+
+    @Override
+    public void onFailure(Exception error) {
+        Log.d("admin view", "Something went off when trying to access the DB." + error.getStackTrace());
+
+    }
+
+    @Override
+    public void onAcceptClick(int position) {
+        stopDataRefresh(); //I do not want the requests to be updated as I am accessing it.
+        Log.d("requests Fragment", "accept click was pressed");
+        long idToAccept = appointments.get(position).getAppointmentID();
+        db.approveSignUpRequest(idToAccept);
+        refreshHandler.post(refreshRunnable);
+    }
+
+    @Override
+    public void onRejectClick(int position) {
+        stopDataRefresh();
+        Log.d("requests Fragment", "reject click was pressed");
+        long idToReject = appointments.get(position).getAppointmentID();
+        db.rejectSignUpRequest(idToReject);
+        refreshHandler.post(refreshRunnable);
+    }
+
+
+    @Override
+    public void onShowMoreClick(int position) {
+        stopDataRefresh();
+        //Get the selected request from the list
+        Appointment selectedAppointment = appointments.get(position);
+        Intent showMoreIntent = new Intent(getActivity(), ShowMoreActivity.class);
+
+        Patient somePatient = (Patient) db.getPatientFromAppointmentID(selectedAppointment.getAppointmentID());
+        showMoreIntent.putExtra("userType", PATIENT);
+        showMoreIntent.putExtra("firstName", somePatient.getFirstName());
+        showMoreIntent.putExtra("lastName", somePatient.getLastName());
+        showMoreIntent.putExtra("email", somePatient.getEmail());
+        showMoreIntent.putExtra("phoneNumber", somePatient.getPhone());
+        showMoreIntent.putExtra("address", somePatient.getAddress());
+        showMoreIntent.putExtra("healthCardNumber", somePatient.getHealthCardNumber());
+
+
+        startActivity(showMoreIntent);
+    }
+}
