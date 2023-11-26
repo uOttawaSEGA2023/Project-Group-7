cdf2bbe5aab8ec818775d0c4841b53c15aa60f35
ARelaxedScholar <angemmanuel.kouakou+professional@gmail.com>
2023-11-26 16:44:53 -0500

    fix(Database): Imported a missing class after merge conflict.

--- app/src/main/java/com/quantumSamurais/hams/database/Database.java
+++ app/src/main/java/com/quantumSamurais/hams/database/Database.java
@@ -17,7 +17,7 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.Transaction;
