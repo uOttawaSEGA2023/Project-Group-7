33a10ae27ed369ee768c1c215147eccac0e678a8
Eknoor004 <141525797+Eknoor004@users.noreply.github.com>
2023-12-01 10:32:34 -0500

    updated classes

---
+++ app/src/main/java/com/quantumSamurais/hams/patient/activities/PatientMainActivity.java
@@ -0,0 +1,24 @@
+package com.quantumSamurais.hams.patient.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.quantumSamurais.hams.R;
+
+public class PatientMainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_main);
+        setup();
+        addListeners();
+    }
+
+    private void setup() {
+
+    }
+    private void addListeners() {
+
+    }
+}
