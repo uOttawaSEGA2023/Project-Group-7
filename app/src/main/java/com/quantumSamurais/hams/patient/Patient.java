43b8ef4a870c31daf69a86e83327c505a87363b3
SplitWolf <mat.jake1360@gmail.com>
2023-12-04 18:03:50 -0500

    fix: Appointments to db no longer crash.

--- app/src/main/java/com/quantumSamurais/hams/patient/Patient.java
+++ app/src/main/java/com/quantumSamurais/hams/patient/Patient.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Patient extends User implements ResponseListener<ArrayList<Doctor>> {
+public class Patient extends User {
 
     private String healthCardNumber;
 
@@ -34,8 +34,6 @@ public class Patient extends User implements ResponseListener<ArrayList<Doctor>>
     public Patient() {
         Database db = Database.getInstance();
         this.appointments = db.getPatientAppointments(this);
-        db.getDoctors(this);
-
     }
 
     //Used during account creation
@@ -46,39 +44,13 @@ public class Patient extends User implements ResponseListener<ArrayList<Doctor>>
         Database db = Database.getInstance();
         db.addSignUpRequest(this);
         date = LocalDate.now();
-        this.getShifts();
     }
 
-    //Used for logins
+    //Used for DEBUGGING
     public Patient(String firstName, String lastName, ArrayList<Integer> hashedPassword, ArrayList<Integer> salt, String emailAddress, String phoneNumber, String postalAddress, String healthCardNumber){
         super(firstName, lastName, hashedPassword, salt, emailAddress, phoneNumber, postalAddress);
         this.healthCardNumber = healthCardNumber;
-        Database db = Database.getInstance();
-        List<Appointment> test= db.getPatientAppointments(this);
-        if(test != null) {
-            this.appointments = test;
-        } else {
-            this.appointments = new ArrayList<>();
-        }
-        date = LocalDate.now();
-        this.getShifts();
-    }
-
-    @Override
-    public void onSuccess(ArrayList<Doctor> data) {
-        List<Appointment> apps = new ArrayList<>();
-//        for(Doctor d: data) {
-//            for(Shift s : d.getShifts()) {
-//                if(s.getStartTime().toLocalDate().equals(date)) {
-//                    // TODO: Add apps for shift
-//                };
-//            }
-//        }
-        this.availableAtDate = apps;
-    }
-    @Override
-    public void onFailure(Exception error) {
-        Log.e("Patient: 70", Objects.requireNonNull(error.getCause()).toString());
+        this.appointments = new ArrayList<>();
     }
     @Override
     public void changeView(Context currentContext) {
@@ -99,8 +71,8 @@ public class Patient extends User implements ResponseListener<ArrayList<Doctor>>
     }
 
     @Exclude
-    public void getShifts() {
-        Database.getInstance().getDoctors(this);
+    public LocalDate getDate() {
+        return date;
     }
 
     public void setAppointmentDate(LocalDate date) {
@@ -108,7 +80,6 @@ public class Patient extends User implements ResponseListener<ArrayList<Doctor>>
     }
     @Exclude
     public List<Appointment> getAppointments() {
-        appointments.add(new Appointment(LocalDateTime.of(2024,12,20,1,2,3),LocalDateTime.of(2024,12,23,2,2,3),new Shift(),this, RequestStatus.PENDING));
         return appointments;
     }
 
