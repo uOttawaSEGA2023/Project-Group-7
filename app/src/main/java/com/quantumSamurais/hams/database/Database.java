02a5f6d89535574207fdf81028b0a23aacac52ce
SplitWolf <mat.jake1360@gmail.com>
2023-12-04 16:00:20 -0500

    feat: Added fields to appointment class, but actually

--- app/src/main/java/com/quantumSamurais/hams/database/Database.java
+++ app/src/main/java/com/quantumSamurais/hams/database/Database.java
@@ -570,7 +570,7 @@ public class Database {
         Supplier<List<Appointment>> query = () -> {
             DocumentReference softwareRef = db.collection("users").document("software");
             try {
-                QuerySnapshot snap = await(softwareRef.collection("appointments").whereEqualTo("myPatient",patient).get());
+                QuerySnapshot snap = await(softwareRef.collection("appointments").whereEqualTo("patient",patient).get());
                 List<Appointment> apps = new ArrayList<>();
                 for (QueryDocumentSnapshot document : snap) {
                     apps.add(document.toObject(Appointment.class));
@@ -587,9 +587,8 @@ public class Database {
             return appList.get();
         } catch (ExecutionException | InterruptedException e) {
             //TODO: Better Error handling
-           // throw new RuntimeException(e);
+           throw new RuntimeException(e);
         }
-        return null;
     }
 
     public CompletableFuture<Shift> getShift(long shiftID) {
